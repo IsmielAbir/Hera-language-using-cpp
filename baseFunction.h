8912838c97dfd0b33d05a798a3ff646d54f14454
Ismielabir <ismielabir286@gmail.com>
2022-12-31 21:10:17 +0600

    Endless Coding

--- baseFunction.h
+++ baseFunction.h
@@ -14,6 +14,50 @@ template <typename t> void minimum(t a, t b){
 template <typename t> void CircleArea(t a){
     cout<<3.141596*a*a<<endl;
 }
+//4 prime
+template <typename t> void prime(t a){
+    t c=0;
+    for(int i=2;i<a;i++){
+        if(a%i==0)
+        c=1;
+    }
+    if(c==0)
+    cout<<"Number is prime"<<endl;
+    else
+    cout<<"Number is not prime"<<endl;
+}
+//5 reverse a number
+template <typename t> void reverse(t n){
+    t r,a=0;
+    while(n>0){
+    r = n%10;
+    a=a*10+r;
+    n=n/10;
+    }
+    cout<<a<<endl;
+}
+//6 factorial
+template <typename t> void factorial(t n){
+    int fact = 1;
+    for(int i=2;i<=n;i++){
+        fact=fact*i;
+    }
+    cout<<fact<<endl;
+}
+//7 linear search
+template <typename t> void LinearSearch(t a[],t n,t m){
+    t c,d=0;
+    for(int i=0;i<n;i++){
+        if(a[i]==m){
+        c=m;
+        d=1;
+        cout<<"Found at index: "<<i<<" "<<"place: "<<i+1<<endl;
+        }
+    }
+    if(d==0)
+    cout<<"Not Found"<<endl;
+}
+//8 Binary Search
 
 
 
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "baseFunction.h"
+using namespace std;
+
+int main()
+{
+    
+    int array[]={1,2,5,9};
+    LinearSearch(array, 4, 19);
+
+    return 0;
+}
